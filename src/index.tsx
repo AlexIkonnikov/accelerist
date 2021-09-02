2eabb9947c7a05c7c89df850edf724debbd8919c
Alex <sashaikonnikov55@gmail.com>
2021-09-02 15:03:17 +0600

    feat: Added fonts and installed styled-component

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
+import GlobalFonts from './assets/fonts';
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalFonts/>
   </React.StrictMode>,
   document.getElementById('root')
 );
