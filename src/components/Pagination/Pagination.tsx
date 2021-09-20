7b003238f22ee80c7be22dc4d1869b30649e724d
Alex <sashaikonnikov55@gmail.com>
2021-09-20 13:33:46 +0600

    fix: Fix pagination because incorrect text is displayed

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,36 +1,66 @@
 import React, { FC, HTMLAttributes } from 'react';
 import styled from 'styled-components';
-import { ReactComponent as Arrow} from '../../assets/icons/arrow.svg';
+import { ReactComponent as Arrow } from '../../assets/icons/arrow.svg';
 import { IMeta } from '../../store/company';
 import { AppText } from './../../ui/AppText';
 
 interface PaginationProps extends HTMLAttributes<HTMLDivElement> {
-  meta?: IMeta,
-  onToggle?: (page: number) => void
+  meta?: IMeta;
+  onToggle?: (page: number) => void;
 }
 
-const Pagination: FC<PaginationProps> = ({className, meta, onToggle}) => {
+const Pagination: FC<PaginationProps> = ({ className, meta, onToggle }) => {
   const currentPage = Number(meta?.currentPage);
   const limit = Number(meta?.itemsPerPage);
+  const itemsCount = meta?.itemCount;
   const totalPage = meta?.totalPages;
   const totalItems = meta?.totalItems;
+
+  const LeftBorderRange = () => {
+    if (totalPage === 1) {
+      return `${1}`;
+    } else {
+      return `${1 + (limit * currentPage - limit)}`;
+    }
+  };
+
+  const rightBorderRange = () => {
+    if (totalPage === 1) {
+      return `${itemsCount}`;
+    }
+    if (currentPage === totalPage) {
+      return `${totalItems}`
+    }
+    return `${currentPage * limit}`
+  }
+
   const handleNextPage = () => {
     onToggle && onToggle(currentPage + 1);
-  }
+  };
 
   const handlePrevPage = () => {
     onToggle && onToggle(currentPage - 1);
-  }
+  };
 
-  if (totalPage && totalPage === 1) {
+  if (totalItems === 0) {
     return null;
   }
 
   return (
     <PaginationWrapper className={className}>
-      {currentPage !== 1 && <button onClick={handlePrevPage}><LeftArrow /></button>}
-      <Text type="FootnoteBlack" tagName="span">{1 + (limit * currentPage - limit)}-{(currentPage * limit)} of {totalItems}</Text>
-      {currentPage !== totalPage && <button onClick={handleNextPage}><Arrow/></button>}
+      {currentPage !== 1 && (
+        <button onClick={handlePrevPage}>
+          <LeftArrow />
+        </button>
+      )}
+      <Text type="FootnoteBlack" tagName="span">
+        {LeftBorderRange()}-{rightBorderRange()} of {totalItems}
+      </Text>
+      {currentPage !== totalPage && (
+        <button onClick={handleNextPage}>
+          <Arrow />
+        </button>
+      )}
     </PaginationWrapper>
   );
 };
